ade58029edd4bf547cf0ed21c02846d80c946999
scoop <78777405+sc0op-creator@users.noreply.github.com>
2021-08-01 05:34:11 +0530

    This is the initial commit

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = '8728742384782348273489'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
